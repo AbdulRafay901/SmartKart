270a35f9dd0492aec9eaa55479fd1a08b461e41c
abdul Rafay <abdulrafay700000@gmail.com>
2026-07-02 03:21:20 +0500

    SmartKart Add

---
+++ SmarKart/src/components/NavIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaCartShopping } from "react-icons/fa6";
+
+const NavIcon = () => {
+  return (
+    <div>
+          <FaCartShopping />
+    </div>
+  )
+}
+
+export default NavIcon
---
+++ SmarKart/src/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavIcon from '../components/NavIcon'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+         <NavIcon />
+    </div>
+  )
+}
+
+export default Navbar
