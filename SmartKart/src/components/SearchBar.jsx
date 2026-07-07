404338749689bbbf18ea34de8514a62219aca6cd
abdul Rafay <abdulrafay700000@gmail.com>
2026-07-08 03:10:18 +0500

    f: SearchBar

--- SmartKart/src/components/SearchBar.jsx
+++ SmartKart/src/components/SearchBar.jsx
@@ -3,19 +3,26 @@ import { CiSearch } from "react-icons/ci";
 import { IoMdArrowForward } from "react-icons/io";
 import { useState } from 'react';
 import { useContext } from 'react';
-
-
-
+import ProductContext from './Context/ProductContext'
+import { Product } from './Context/ProductContext'
 
 const SearchBar = () => {
 
 
-  
+  const { Title } = useContext(Product)
+
+  const [search, setsearch] = useState()
+
+   const Search = () => {
+       Title(search)
+   }
 
   return (
     <div className='search-bar'>
          <CiSearch className='search-icon'/>
-         <input type="search" placeholder='Search'></input>
+         <input type="search" placeholder='Search'onChange={(e) => {
+              setsearch(e.target.value)
+         }}></input>
          <div className="arrow-div">
                <IoMdArrowForward className='arrow-icon' onClick={Search}/>
          </div>
